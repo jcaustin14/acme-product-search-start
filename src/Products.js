dd25a50b29bf5325839cd046f5cf44d115e828b0
jcaustin14 <jcaustin14@gmail.com>
2023-04-17 09:12:17 -0500

    adding code to build out framework without search and filter functionality

--- src/Products.js
+++ src/Products.js
@@ -4,8 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Products = ()=> {
   const { products } = useSelector(state => state);
+  const [search, setSearch] = useState('');
+  const [checkbox, setCheckbox] = useState(false);
+
   return (
     <div>
+      <div id='searchAndCheckBoxDiv'>
+        <div id='searchDiv'>
+          <input placeholder='search by name' onChange={ev => setSearch(ev.target.value)}></input>
+        </div>
+        <div id='checkBoxDiv'>
+          <label htmlFor='checkBox'>in stock only</label>
+          <input id='checkBox' type='checkbox' onChange={ev => setCheckbox(!checkbox)}></input>
+        </div>
+      </div>
       <ul>
         {
           products.map( product => {
